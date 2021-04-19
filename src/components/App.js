e524b64aa236828b1ea53b69ce636193b7b6c119
kandari012 <73176498+kandari012@users.noreply.github.com>
2021-04-19 21:59:35 +0530

    adding UI for POSTS list

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions/posts";
+import { PostsList } from "./";
 
 export class App extends Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
   }
   render() {
+    const { posts } = this.props;
     console.log(this.props);
-    return <div>App</div>;
+    return (
+      <div>
+        <PostsList posts={posts} />
+      </div>
+    );
   }
 }
 
