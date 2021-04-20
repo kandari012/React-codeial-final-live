74322fe61d8a1adff8e971686906b32712a9ff4a
kandari012 <73176498+kandari012@users.noreply.github.com>
2021-04-20 21:56:09 +0530

    form handling :uncontrolled components

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,21 +1,42 @@
 import React, { Component } from "react";
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.emailInputRef = React.createRef(); // reference for emailinput
+    this.passwodInputRef = React.createRef(); //when we submit form we get the value of input field in attached reference
+  }
+  // on button click will prevent submit default prop
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.emailInputRef);
+    console.log(this.passwodInputRef);
+  };
   render() {
     return (
       <form className="login-form">
         <span className="login-signup-header">Register</span>
         <div className="field">
-          <input type="email" placeholder="Email" required />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            ref={this.emailInputRef}
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="Password" required />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            ref={this.passwodInputRef}
+          />
         </div>
         <div className="field">
           <input type="string" placeholder="name" required />
         </div>
         <div className="field">
-          <button>Register</button>
+          <button onClick={this.handleFormSubmit}>Register</button>
         </div>
       </form>
     );
